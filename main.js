a09ca05262714f38029241a4bce1f2a22adc1372
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-07-21 11:23:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,17 @@ async function getFetchData() {
   const data = await response.json();
   console.log("Data: ", data);
   displayData(data)
+  return data;
 }
 
 function displayData(data){
-  
+  let ImgSection = document.querySelector(".img-section");
+  data.map((item) => {
+    let ImgContainer = document.createElement('div')
+    ImgContainer.classList.add('img-container')
+    ImgContainer.innerHTML = `
+      <img src="${item.urls.regular}" alt="">
+    `
+    ImgSection.appendChild(ImgContainer)
+  })
 }
